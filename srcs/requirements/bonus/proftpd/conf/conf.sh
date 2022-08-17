9557180d1509eafde1ca82c87b53e7dfb2d8ba84
soleraeus <25184475+soleraeus@users.noreply.github.com>
2022-08-17 16:44:05 +0200

    FTP server functional

--- srcs/requirements/bonus/proftpd/conf/conf.sh
+++ srcs/requirements/bonus/proftpd/conf/conf.sh
@@ -1,9 +1,18 @@
 #! /bin/bash
 
-if [ ! id -u ${FTP_USER} &> /dev/null ]
-then
-	useradd ${FTP_USER} --password=${FTP_PWD}
-	groupadd ftpuser
-	usermod -aG ftpuser ${FTP_USER}
-fi
+until test -f /wordpress/index.php; do
+	sleep 1
+done
+useradd ${FTP_USER} --base-dir /wordpress
+echo "${FTP_USER}:${FTP_PWD}" | chpasswd
+groupadd www-data
+usermod -aG www-data ${FTP_USER}
+sed -i "s/proftpduser/${FTP_USER}/g" "/etc/proftpd/proftpd.conf"
+sed -i "s/proftpduser/${FTP_USER}/g" "/etc/proftpd/conf.d/Inception.conf"
+
+sed -i "s/proftpdgroup/www-data/g" "/etc/proftpd/proftpd.conf"
+sed -i "s/proftpdgroup/www-data/g" "/etc/proftpd/conf.d/Inception.conf"	
+find /wordpress -type d -print0 | xargs -0 chmod 775
+find /wordpress -type f -print0 | xargs -0 chmod 664
+
 exec proftpd -n -q
